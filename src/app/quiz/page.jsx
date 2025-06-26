34934a2ce051187eb5e48877b7853e107a30b2e7
sajeela123 <U2486893@rts-365.dk>
2025-06-26 11:15:59 +0200

    add

--- src/app/quiz/page.jsx
+++ src/app/quiz/page.jsx
@@ -79,3 +79,4 @@ export default function QuizPage() {
     </div>
   );
 }
+
--- src/app/result/page.jsx
+++ src/app/result/page.jsx
@@ -1,24 +1,28 @@
-'use client';
-import { useSearchParams, useRouter } from 'next/navigation';
-
+'use client'
+ 
+import { useRouter, useSearchParams } from 'next/navigation'
+ 
 export default function ResultPage() {
-  const params = useSearchParams();
-  const router = useRouter();
-  const score = params.get('score');
-  const total = params.get('total');
-
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const score = searchParams.get('score')
+  const total = searchParams.get('total')
+ 
+ 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-green-100 text-center p-6">
-      <h2 className="text-3xl font-bold text-green-700 mb-4">Resultat 🎉</h2>
+ 
+   
+    <div className="max-w-xl w-full h-200 bg-indigo-100 to-purple-200shadow rounded-lg p-16 flex flex-col items-center justify-center text-center m-auto">
+      <h2 className="text-3xl font-bold text-green-700 mb-4">Result</h2>
       <p className="text-lg mb-6">
-        Du fik <strong>{score}</strong> ud af <strong>{total}</strong> rigtige!
+        You have <strong>{score}</strong> out of <strong>{total}</strong> correct answers!
       </p>
       <button
         onClick={() => router.push('/')}
         className="px-6 py-3 bg-green-600 hover:bg-green-700 text-white rounded-lg font-medium transition"
       >
-        Prøv en ny quiz
+        Start new  quiz
       </button>
     </div>
-  );
+  )
 }
